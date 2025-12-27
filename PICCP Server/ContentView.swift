81dd1aae66827910fce53d449cc7e26347ebf105
Widmer <115965339+crazywidmer@users.noreply.github.com>
2025-12-27 20:37:15 -0300

    Initial Commit

---
+++ PICCP Server/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PICCP Server
+//
+//  Created by Luiz Fernando Widmer Neto on 27/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PICCP Server/PICCP_ServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PICCP_ServerApp.swift
+//  PICCP Server
+//
+//  Created by Luiz Fernando Widmer Neto on 27/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PICCP_ServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
